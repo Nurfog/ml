25d0f83378acc44abcf41d1fabfff48c5cd03969
Nurfog <juan.allende@gmail.com>
2025-08-14 16:37:26 -0400

    actualizacion

--- cms/forms.py
+++ cms/forms.py
@@ -205,3 +205,53 @@ class ColegioForm(forms.ModelForm):
         email = self.cleaned_data['email']
         return email.lower()
         
+
+class PerfilForm(forms.ModelForm):
+    class Meta:
+        model =  
+        fields = ['about', 'trabajo', 'rut', 'nombres', 'ap_paterno', 'ap_materno', 'id_pais', 'id_region', 'id_comuna', 'direccion', 'telefono', 'celular', 'foto',
+                  'socialgit', 'socialx', 'socialfb', 'socialig', 'socialyt', 'socialli']        
+        widgets = {
+            'about': forms.Textarea(attrs={'class': 'form-control', 'rows': 5, 'placeholder': 'Escribe algo sobre ti...'}),
+            'trabajo': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Cual es tu cargo en la empresa?'}),
+            'rut': forms.TextInput(attrs={'class': 'form-control'}),
+            'nombres': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Nombres'}),
+            'ap_paterno': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Apellido Paterno'}),
+            'ap_materno': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Apellido Materno'}),
+            'id_pais': forms.Select(attrs={'class': 'form-control', 'placeholder': 'Selecciona un país'}),
+            'id_region': forms.Select(attrs={'class': 'form-control', 'placeholder': 'Selecciona una región'}),
+            'id_comuna': forms.Select(attrs={'class': 'form-control', 'placeholder': 'Selecciona una comuna'}),
+            'direccion': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Dirección'}),
+            'telefono': forms.TextInput(attrs={'class': 'form-control', 'placeholder': '+56212345678'}),
+            'celular': forms.TextInput(attrs={'class': 'form-control', 'placeholder': '+56912345678'}),
+            'foto': forms.ClearableFileInput(attrs={'class': 'form-control-file', 'placeholder': 'Selecciona una foto de perfil'}),
+            'socialgit': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'URL de tu perfil de GitHub'}),
+            'socialx': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'URL de tu perfil de X'}),
+            'socialfb': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'URL de tu perfil de Facebook'}),            
+            'socialig': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'URL de tu perfil de Instagram'}),
+            'socialyt': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'URL de tu canal de YouTube'}),
+            'socialli': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'URL de tu perfil de LinkedIn'})
+        }
+    def clean_rut(self):
+        rut = self.cleaned_data['rut']
+        return rut.upper()
+    
+    def clean_nombre(self):
+        nombre = self.cleaned_data['nombre']
+        return nombre.capitalize()
+    
+    def clean_apellido(self):
+        apellido = self.cleaned_data['apellido']
+        return apellido.capitalize()
+
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        return email.lower()
+    
+    def clean_telefono(self):
+        telefono = self.cleaned_data['telefono']
+        return telefono.upper()
+    
+    def clean_direccion(self):
+        direccion = self.cleaned_data['direccion']
+        return direccion.capitalize()
--- core/settings.py
+++ core/settings.py
@@ -31,10 +31,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = env('Secret_key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["192.168.0.254", "127.0.0.1", "localhost"]
 
 
 # Application definition
